37ac56fd1b82d10da5537e8fe4438fb29f759e62
Arif Nur Ramadhan <91766087+iifrmdn@users.noreply.github.com>
2022-04-08 16:40:22 +0700

    Pengisian Nilai dengan Assignment

---
+++ Pengisian Nilai dengan Assignment/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+
+    i = 10;
+    printf("Nilai i = %d \n", i);
+
+    printf("Nilai setelah diperbarui: \n");
+    i = i + 1;
+    printf("Niali i = %d \n", i);
+
+
+
+    return 0;
+}
